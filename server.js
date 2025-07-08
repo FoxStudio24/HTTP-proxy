e43eea399e0c3599767d086db604588a8f824370
FoxStudio <133024877+FoxStudio24@users.noreply.github.com>
2025-07-08 10:59:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// server.js
 const http = require('http');
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: req.url, changeOrigin: true }, (e) => {
-    res.writeHead(500);
-    res.end('Proxy error: ' + e.message);
+  proxy.web(req, res, { target: req.url, changeOrigin: true }, (err) => {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Proxy error: ' + err.message);
   });
 });
 
-server.listen(process.env.PORT || 8080, () => {
-  console.log('Proxy server running');
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Proxy running on port ${PORT}`);
 });
